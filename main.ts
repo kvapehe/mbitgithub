54d67611d6a8cc8c4b42391efd49495be2bf63ff
kvapehe <33888633+kvapehe@users.noreply.github.com>
2021-09-12 17:28:04 +0200

    Klar til innlevering

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+})
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . . . .
+    # . . . #
+    # # # # #
+    `)
+basic.forever(function () {
+    music.ringTone(131)
+    basic.pause(40)
+    music.rest(music.beat(BeatFraction.Whole))
+    basic.pause(6000)
+})
