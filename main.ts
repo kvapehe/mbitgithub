ab5730e58d93d00a4c7e3a3b78112f53729b830a
kvapehe <33888633+kvapehe@users.noreply.github.com>
2021-09-12 19:00:22 +0200

    Klar til innlevering

--- main.ts
+++ main.ts
@@ -1,22 +1,61 @@
+/**
+ * Unngå å telle forbi 9. pga. "scrolling", og test med modulus..
+ * 
+ * Teller opp med A og ned med B
+ * 
+ * Telle opp reset til 0
+ * 
+ * Telle ned reset til 9
+ * 
+ * Lurt å teste på enkle ting for å brukes senere.
+ * 
+ * Lyden er med for å se det er liv i Mbit.
+ * 
+ * Endrer nok intervall etter hvert.
+ */
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showIcon(IconNames.Happy)
+    tallet += 1
+    if (tallet % startigjen0 == 0) {
+        tallet = 0
+        basic.showLeds(`
+            # # # # #
+            . . . . #
+            . # . . #
+            # # # # #
+            . # . . .
+            `)
+        basic.pause(100)
+    } else {
+        basic.clearScreen()
+    }
+    basic.showNumber(tallet)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.No)
+    tallet += -1
+    if (tallet % startigjen0 == 0) {
+        tallet = 9
+        basic.showLeds(`
+            # # # # #
+            # . . . .
+            # . . # .
+            # # # # #
+            . . . # .
+            `)
+        basic.pause(100)
+    } else {
+        basic.clearScreen()
+    }
+    basic.showNumber(tallet)
 })
-basic.showLeds(`
-    # . . . #
-    . # . # .
-    . . . . .
-    # . . . #
-    # # # # #
-    `)
+let startigjen0 = 0
+let tallet = 0
+tallet = 0
+startigjen0 = 10
+let startigjen10 = 10
+basic.showNumber(tallet)
 basic.forever(function () {
     music.ringTone(262)
-    basic.pause(40)
+    basic.pause(25)
     music.rest(music.beat(BeatFraction.Whole))
-    basic.pause(6000)
+    basic.pause(9000)
 })
